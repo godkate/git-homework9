79a69b2df32fe4965ba1c1f94a30f1e59dd0e012
godkate <bogodul883@gmail.com>
2022-10-23 11:18:38 -0400

    Added eighth script

--- task8.js
+++ task8.js
@@ -1,1 +1,10 @@
 //Вивести всі числа в діапазоні від 100 до 200 кратні 3
+
+console.log('start task8')
+let eighthNum = 100;
+for (; eighthNum <= 200; eighthNum += 1) {
+    if (eighthNum % 3 === 0) {
+        console.log(eighthNum);
+    }
+}
+console.log('end task8')
