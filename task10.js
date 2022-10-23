76d384b53446d68bbfdfbb936e0d5c80bf5a5406
godkate <bogodul883@gmail.com>
2022-10-23 12:22:47 -0400

    Added tenth script

--- task10.js
+++ task10.js
@@ -1,1 +1,10 @@
 //Надрукувати повну таблицю множення від 1 до 10
+console.log('start task10')
+let number = 0;
+for (a = 1; a < 10; a++) {
+    for (b = 1; b < 10; b++){
+        number = a*b;
+        console.log(`${a}*${b}=${number}`)
+    }
+}
+console.log('end task10')
